e91917de71d2c0e9776b82d25540ad11baa0a31b
WOONJANG <104566843+WOONJANG@users.noreply.github.com>
2026-02-11 19:30:01 +0900

    Update clubs.js

--- assets/js/clubs.js
+++ assets/js/clubs.js
@@ -65,31 +65,31 @@ window.CLUBS = [
       { text: "Collaboration", tone: "accent" },
       { text: "MD", tone: "normal" },
     ]
-  },
+  }
+  ,
   {
-  name: "박은태",
-  store: "스페셜 카드",
-  image: "/assets/img/park-euntae.png",
-  logo: "/assets/img/park-euntae.png",
-  url: "https://namu.wiki/w/%EB%B0%95%EC%9D%80%ED%83%9C",
-  hidden: true,
-  revealKey: "박은태"
-},
-
+    name: "박은태",
+    store: "박은태",
+    image: "/assets/img/park-euntae.png",
+    logo: "/assets/img/park-euntae.png",
+    url: "https://namu.wiki/w/%EB%B0%95%EC%9D%80%ED%83%9C",
+    hidden: true,
+    revealKey: "박은태",
+    tags: [
+      { text: "Hidden", tone: "accent" }
+    ]
+  }
+  ,
   {
-  name: "재희",
-  store: "스페셜 카드",
-  image: "/assets/img/jaehee.png",
-  logo: "/assets/img/jaehee.png",
-  url: "https://namu.wiki/w/%EC%9E%AC%ED%9D%AC(NCT)",
-  hidden: true,
-  revealKey: "재희"
-},
+    name: "재희",
+    store: "재희",
+    image: "/assets/img/park-euntae.png",
+    logo: "/assets/img/park-euntae.png",
+    url: "https://namu.wiki/w/%EC%9E%AC%ED%9D%AC(NCT)",
+    hidden: true,
+    revealKey: "재희",
+    tags: [
+      { text: "Hidden", tone: "accent" }
+    ]
+  }
 ];
-
-
-
-
-
-
-
