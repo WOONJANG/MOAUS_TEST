f5cee9912c40280a4a6d4d195f770ff461a65718
WOONJANG <104566843+WOONJANG@users.noreply.github.com>
2026-02-11 19:43:31 +0900

    Update clubs.js

--- assets/js/clubs.js
+++ assets/js/clubs.js
@@ -2,8 +2,8 @@ window.CLUBS = [
   {
     name: "V-EXX",
     store: "브이엑스 공식 스토어",
-    image: "/assets/img/vexx_bgimg.png",
-    logo: "/assets/img/vexx_logo.png",
+    image: "./assets/img/vexx_bgimg.png",
+    logo: "./assets/img/vexx_logo.png",
     url: "https://vexxsports.com/",
     tags: [
       { text: "Official", tone: "accent" },
@@ -13,8 +13,8 @@ window.CLUBS = [
   {
     name: "용인 FC",
     store: "용인 FC 스토어",
-    image: "/assets/img/yongin_bgimg.png",
-    logo: "/assets/img/yongin_logo.png",
+    image: "./assets/img/yongin_bgimg.png",
+    logo: "./assets/img/yongin_logo.png",
     url: "https://yonginfcstore.com/",
     tags: [
       { text: "Football", tone: "accent" },
@@ -24,8 +24,8 @@ window.CLUBS = [
   {
     name: "안양 정관장 레드부스터스",
     store: "정관장 레드부스터스 팀스토어",
-    image: "/assets/img/jkj_bgimg.png",
-    logo: "/assets/img/jkj_logo.png",
+    image: "./assets/img/jkj_bgimg.png",
+    logo: "./assets/img/jkj_logo.png",
     url: "https://redboostersstore.com/",
     tags: [
       { text: "Basketball", tone: "accent" },
@@ -36,8 +36,8 @@ window.CLUBS = [
   {
     name: "KBO X DISNEY",
     store: "한국프로야구 X 디즈니",
-    image: "/assets/img/kbo_disney_bgimg.png",
-    logo: "/assets/img/kbo_disney_logo.png",
+    image: "./assets/img/kbo_disney_bgimg.png",
+    logo: "./assets/img/kbo_disney_logo.png",
     url: "https://moaus.shop/",
     tags: [
       { text: "Collaboration", tone: "accent" },
@@ -47,8 +47,8 @@ window.CLUBS = [
   {
     name: "KBL X MARVEL",
     store: "한국프로농구 X 마블",
-    image: "/assets/img/kbl_marvel_bgimg.png",
-    logo: "/assets/img/kbl_marvel_logo.png",
+    image: "./assets/img/kbl_marvel_bgimg.png",
+    logo: "./assets/img/kbl_marvel_logo.png",
     url: "https://moaus.shop/",
     tags: [
       { text: "Collaboration", tone: "accent" },
@@ -58,8 +58,8 @@ window.CLUBS = [
   {
     name: "Dr.B-Pang",
     store: "닥터비팡",
-    image: "/assets/img/drbp_bgimg.png",
-    logo: "/assets/img/drbp_logo.png",
+    image: "./assets/img/drbp_bgimg.png",
+    logo: "./assets/img/drbp_logo.png",
     url: "https://moaus.shop/",
     tags: [
       { text: "Collaboration", tone: "accent" },
@@ -70,8 +70,8 @@ window.CLUBS = [
   {
     name: "박은태",
     store: "박은태",
-    image: "/assets/img/park-euntae.png",
-    logo: "/assets/img/park-euntae.png",
+    image: "./assets/img/park-euntae.png",
+    logo: "./assets/img/park-euntae.png",
     url: "https://namu.wiki/w/%EB%B0%95%EC%9D%80%ED%83%9C",
     hidden: true,
     revealKey: "박은태",
@@ -83,8 +83,8 @@ window.CLUBS = [
   {
     name: "재희",
     store: "재희",
-    image: "/assets/img/park-euntae.png",
-    logo: "/assets/img/park-euntae.png",
+    image: "./assets/img/park-euntae.png",
+    logo: "./assets/img/park-euntae.png",
     url: "https://namu.wiki/w/%EC%9E%AC%ED%9D%AC(NCT)",
     hidden: true,
     revealKey: "재희",
@@ -93,3 +93,4 @@ window.CLUBS = [
     ]
   }
 ];
+
